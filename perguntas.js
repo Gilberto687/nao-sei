b78b0a42f24bc31eb7150979f32a075db7c2acf6
Gilberto687 <gilberto.eduardo.oliveira@escola.pr.gov.br>
2025-06-11 11:46:56 -0300

    11/06

--- perguntas.js
+++ perguntas.js
@@ -11,13 +11,13 @@ criaCartao(
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
+    'Futebol',
+    'Quem é o maior clube brasileiro com mais campeonato mundial?',
     'Uma função é um bloco de código que executa alguma tarefa'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Futebol',
+    'Quem é o maior clube com titulo nacional?',
+    'Palmeiras'
 )
